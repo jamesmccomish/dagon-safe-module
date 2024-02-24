f6df66e6c502d55f0d3a3a9241ccff68b7f45fb6
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-02-24 12:12:13 -0700

    chore: add checks to safe setup test

--- test/DagonTokenModule.t.sol
+++ test/DagonTokenModule.t.sol
@@ -8,20 +8,31 @@ import "forge-std/console.sol";
 
 contract DagonTokenModuleTest is BasicTestConfig, SafeTestConfig {
     Safe safe;
-    address safeAddr;
+    address safeAddress;
 
     function setUp() public {
-        safeAddr = address(safeProxyFactory.createProxyWithNonce(address(safeSingleton), new bytes(0), uint256(1)));
-        safe = Safe(payable(safeAddr));
+        safeAddress = address(safeProxyFactory.createProxyWithNonce(address(safeSingleton), new bytes(0), uint256(1)));
+        safe = Safe(payable(safeAddress));
 
-        address[] memory owners = new address[](1);
+        address[] memory owners = new address[](2);
         owners[0] = alice;
+        owners[1] = bob;
 
         // initialize safe with basic owners and threshold
         safe.setup(owners, 1, address(0), new bytes(0), address(0), address(0), 0, payable(address(0)));
     }
 
-    function test_setup() public {
-        console.logAddress(address(safe));
+    function test_setupSafe() public {
+        // Check that the account from setup is deployed and data is set on account, and safe
+        assertEq(safe.getThreshold(), 1, "threshold not set");
+        assertEq(safe.getOwners()[0], alice, "owner not set on safe");
+        assertEq(safe.getOwners()[1], bob, "owner not set on safe");
+
+        address[] memory owners = new address[](2);
+        owners[0] = alice;
+        owners[1] = bob;
+        // Can not initialize the same account twice
+        vm.expectRevert("GS200");
+        safe.setup(owners, 1, address(0), new bytes(0), address(0), address(0), 0, payable(address(0)));
     }
 }
